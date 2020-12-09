eb000baa2837d25e89616c25132341dbbd253a24
Diego Ramires <goncalves.ramires@gmail.com>
2020-12-09 07:54:35 -0300

    add: install and configure eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 
 type Props = {
   title: string
 }
-const a = 'asdad'
-export default function Home({ title = "React Avançado!" }: Props ) {
+
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,12 +14,11 @@ export default function Home({ title = "React Avançado!" }: Props ) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          {title}
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing
+          {' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -60,10 +59,11 @@ export default function Home({ title = "React Avançado!" }: Props ) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by
+          {' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
