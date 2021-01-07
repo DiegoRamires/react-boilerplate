a94fd1fb550483e83abbd05d23fad99da4e26a3c
Diego Ramires <goncalves.ramires@gmail.com>
2021-01-07 18:51:33 -0300

    update: refactor storybook to use new feature `controls` instead knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
-    '@storybook/addon-actions',
     '@storybook/addon-essentials',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs/register'
   ]
 }
